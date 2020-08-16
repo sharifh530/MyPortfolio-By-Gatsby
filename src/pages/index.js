3e0acad07ad8924a82e8d184150e8a69a416cc55
Sazzad Hossain <sharifh530@gmail.com>
2020-08-17 03:15:38 +0600

    Intro component implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
-import Hero from "../components/Hero"
+import Intro from "../components/Intro"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 export default () => {
-  return <Layout>index page</Layout>
+  return (
+    <Layout>
+      <Intro />
+    </Layout>
+  )
 }
 // ...GatsbyImageSharpFluid
